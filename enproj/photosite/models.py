4ab48db2a21d45762df05f3bc101599500090bed
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2020-12-15 12:10:33 +0300

    Add files via upload

--- enproj/photosite/models.py
+++ enproj/photosite/models.py
@@ -4,15 +4,13 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
-
-# Create your models here.
 class categories(models.Model):
-    slug = models.SlugField(verbose_name='Пермалинк', max_length=50, unique=True)
-    order = models.IntegerField(verbose_name='Порядок', null=True, blank=True)
-    name = models.CharField(max_length=127, verbose_name='Название категории', blank=True)
-    description = models.TextField(max_length=800, verbose_name='Описание категории', blank=True)
+    slug = models.SlugField(verbose_name='Permalink', max_length=50, unique=True)
+    order = models.IntegerField(verbose_name='Order', null=True, blank=True)
+    name = models.CharField(max_length=127, verbose_name='Title', blank=True)
+    description = models.TextField(max_length=800, verbose_name='Category description', blank=True)
     cover = models.ImageField(
-        upload_to='uploaded/catcovers/', verbose_name='Обложка категории', max_length=200, blank=True
+        upload_to='uploaded/catcovers/', verbose_name='Category cover', max_length=200, blank=True
     )
 
     def __str__(self):
@@ -23,31 +21,31 @@ class categories(models.Model):
         self.save()
 
     class Meta:
-        verbose_name = 'категорию'
-        verbose_name_plural = 'категории'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 
 class post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор', default='root')
-    slug = models.SlugField(verbose_name='Пермалинк', max_length=50, unique=True)
-    order = models.IntegerField(verbose_name='Порядок', null=True, blank=True)
-    name = models.CharField(max_length=127, verbose_name='Заголовок')
-    description = models.TextField(max_length=800, verbose_name='Описание', blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Author', default='root')
+    slug = models.AutoField(primary_key=True)
+    order = models.IntegerField(verbose_name='Order', null=True, blank=True)
+    name = models.CharField(max_length=127, blank=True)
+    description = models.TextField(max_length=800, verbose_name='Description', blank=True)
     category = models.ForeignKey(
         categories,
-        verbose_name='Категория',
+        verbose_name='Category',
         on_delete=models.CASCADE,
         blank=True,
         null=True,
         related_name='postcategory',
-        default=5
+        default=8,
     )
     image = models.ImageField(
-        upload_to='uploaded/images/', verbose_name='Изображение', max_length=200
+        upload_to='uploaded/images/', verbose_name='Image', max_length=200
     )
-    videolink = models.URLField(default='', verbose_name='Ссылка на видео', blank=True)
-    created_date = models.DateTimeField(default=timezone.now, verbose_name='Дата/время создания')
-    status = models.BooleanField(default=True, verbose_name='Опубликовано')
+    videolink = models.URLField(default='', verbose_name='Video link', blank=True)
+    created_date = models.DateTimeField(default=timezone.now, verbose_name='Published')
+    status = models.BooleanField(default=True, verbose_name='Published')
 
     def __str__(self):
         return self.name
@@ -57,43 +55,43 @@ class post(models.Model):
         self.save()
 
     class Meta:
-        verbose_name = 'фото запись'
-        verbose_name_plural = 'фото записи'
+        verbose_name = 'Photo item'
+        verbose_name_plural = 'Photo item'
 
 
 class userprofile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
-    description = models.TextField(max_length=400, default='', verbose_name='Обо мне', blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User')
+    description = models.TextField(max_length=400, default='', verbose_name='About me', blank=True)
     avatar = models.ImageField(
-        upload_to='uploaded/avatars/', verbose_name='Аватар', max_length=200, blank=True
+        upload_to='uploaded/avatars/', verbose_name='Profile image', max_length=200, blank=True
     )
     cover = models.ImageField(
-        upload_to='uploaded/usercovers/', verbose_name='Обложка', max_length=200, blank=True
+        upload_to='uploaded/usercovers/', verbose_name='Profile cover', max_length=200, blank=True
     )
     vklink = models.URLField(default='', max_length=254, verbose_name='VK', blank=True)
     fblink = models.URLField(default='', max_length=254, verbose_name='Facebook', blank=True)
     tglink = models.URLField(default='', max_length=254, verbose_name='Telegram', blank=True)
     email = models.EmailField(default='', max_length=254, verbose_name='E-mail', blank=True)
-    sitename = models.CharField(default='', max_length=127, blank=True, verbose_name='Название сайта')
-    sitesubname = models.CharField(default='', max_length=127, blank=True, verbose_name='Подзаголовок сайта')
+    sitename = models.CharField(default='', max_length=127, blank=True, verbose_name='Website title')
+    sitesubname = models.CharField(default='', max_length=127, blank=True, verbose_name='Website sub-title')
     icon = models.FileField(
         default='', upload_to='uploaded/favicon/', max_length=254, blank=True, verbose_name='Favicon'
     )
     logobig = models.FileField(
-        default='', upload_to='uploaded/logobig/', max_length=254, blank=True, verbose_name='Логотип сайта Retina'
+        default='', upload_to='uploaded/logobig/', max_length=254, blank=True, verbose_name='Retina logo'
     )
     logo = models.FileField(
-        default='', upload_to='uploaded/logo/', max_length=254, blank=True, verbose_name='Логотип сайта'
+        default='', upload_to='uploaded/logo/', max_length=254, blank=True, verbose_name='Logo'
     )
     copyright = models.CharField(default='', max_length=127, blank=True, verbose_name='Copyright')
-    customcss = models.TextField(default='', max_length=127, blank=True, verbose_name='Кастомные CSS')
+    customcss = models.TextField(default='', max_length=127, blank=True, verbose_name='Custom CSS')
 
     def __str__(self):
         return self.user.username
 
     class Meta:
-        verbose_name = 'глобальные настройки'
-        verbose_name_plural = 'глобальные настройки'
+        verbose_name = 'Global Settings'
+        verbose_name_plural = 'Global Settings'
 
 
 def create_profile(sender, **kwargs):
